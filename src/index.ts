b50ca129c12ace11babf7f713b06206769ba6024
Raphael Passos <raphael251@hotmail.com>
2025-07-25 11:36:05 -0300

    chore: add environment variables parsing
    Added zod lib to validate the schema for the environment variables, to guarantee that all variables are configured in the envs on startup and avoid errors related to missing envs, increasing the quality of the application

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,50 @@
+import 'dotenv/config';
 import express from 'express';
 import { UsersSignUpController } from './modules/users/http/controllers/sign-up';
 import { DataSource } from 'typeorm';
 import { UserRepository } from './modules/users/repositories';
 import { Hasher } from './modules/shared/security/hasher';
+import { AppConfigLoader } from './modules/shared/configs/app-config';
 
-const app = express();
-export const appDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'admin',
-  password: '123456',
-  database: 'users-db',
-  entities: [__dirname + '/modules/**/*.{js,ts}'],
-  synchronize: true, // Set to false in production
-  logging: true,
-})
+async function startApplication() {
+  const appConfig = AppConfigLoader.load();
 
-appDataSource.initialize()
-  .then(() => {
-    console.log('Data Source has been initialized!');
+  const appDataSource = new DataSource({
+    type: 'postgres',
+    host: appConfig.DB_HOST,
+    port: appConfig.DB_PORT,
+    username: appConfig.DB_USERNAME,
+    password: appConfig.DB_PASSWORD,
+    database: appConfig.DB_DATABASE,
+    entities: [__dirname + '/modules/**/*.{js,ts}'],
+    synchronize: true, // Set to false in production
+    logging: true,
   })
-  .catch((err) => {
-    console.error('Error during Data Source initialization:', err);
+
+  const app = express();
+
+  appDataSource.initialize()
+    .then(() => {
+      console.log('Data Source has been initialized!');
+    })
+    .catch((err) => {
+      console.error('Error during Data Source initialization:', err);
+    });
+
+  app.use(express.json());
+
+  app.get('/', (req, res) => {
+    res.send('Welcome to the Movie Tickets Reservation System!');
   });
 
-app.use(express.json());
+  app.post('/users', (req, res) => new UsersSignUpController(new UserRepository(), new Hasher()).handle(req, res));
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Movie Tickets Reservation System!');
-});
+  app.listen(appConfig.SERVER_PORT, () => {
+    console.log(`Server is running on http://localhost:${appConfig.SERVER_PORT}`);
+  });
+}
 
-app.post('/users', (req, res) => new UsersSignUpController(new UserRepository(), new Hasher()).handle(req, res));
-
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+startApplication().catch((error) => {
+    console.error('Error starting the application:', error);
+    process.exit(1);
 });
---
+++ src/modules/shared/configs/app-config.ts
@@ -0,0 +1,28 @@
+import zod from "zod";
+
+/**
+ * AppConfigLoader is responsible for loading and validating the application configuration
+ * from environment variables using Zod for schema validation.
+ */
+export class AppConfigLoader {
+  static load() {
+    const envSchema = zod.object({
+      SERVER_PORT: zod.string(),
+
+      DB_USERNAME: zod.string(),
+      DB_PASSWORD: zod.string(),
+      DB_DATABASE: zod.string(),
+      DB_HOST: zod.string(),
+      DB_PORT: zod.coerce.number(),
+    });
+
+    const envParsingResult = envSchema.safeParse(process.env);
+
+    if (!envParsingResult.success) {
+      console.error('Invalid environment variables:', JSON.stringify(zod.treeifyError(envParsingResult.error)));
+      process.exit(1);
+    }
+
+    return envParsingResult.data;
+  }
+}
