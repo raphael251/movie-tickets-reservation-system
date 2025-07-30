5de17a530cfd324fcec43002eb561f0ba9710ee2
Raphael Passos <raphael251@hotmail.com>
2025-07-29 22:22:29 -0300

    chore: add express auth middleware

---
+++ src/modules/shared/external/express/middlewares/auth-middleware.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import { ITokenValidator } from '../../../security/interfaces/token-validator';
+
+export function expressAuthMiddleware(tokenValidator: ITokenValidator) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const validationResult = tokenValidator.validate(token);
+
+    if (!validationResult) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    next();
+  };
+}
