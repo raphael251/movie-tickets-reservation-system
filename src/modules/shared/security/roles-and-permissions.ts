84cc5f93caa2f53fa12a1554b313ffb4c52de803
Raphael Passos <raphael251@hotmail.com>
2025-08-01 00:23:19 -0300

    chore: update regular users permissions list

--- src/modules/shared/security/roles-and-permissions.ts
+++ src/modules/shared/security/roles-and-permissions.ts
@@ -1,4 +1,4 @@
 export const rolesAndPermissions: Record<string, Set<string>> = {
   admin: new Set(['movies:create', 'movies:read', 'movies:update', 'movies:delete']),
-  regular: new Set(),
+  regular: new Set(['reservations:create', 'reservations:read', 'movies:read']),
 };
