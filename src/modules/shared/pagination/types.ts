699a4bee83c9d3ffe314f47e508480836f482291
Raphael Passos <raphael251@hotmail.com>
2025-08-23 01:38:09 -0300

    chore(movies): add paginations default limit to repository

--- src/modules/shared/pagination/types.ts
+++ src/modules/shared/pagination/types.ts
@@ -1,7 +1,7 @@
 export interface IPaginationParams {
   before?: string;
   cursor?: string;
-  limit: number;
+  limit?: number;
 }
 
 export type TPaginationResponse<T> = { data: T[]; hasNext: true; nextCursor: string } | { data: T[]; hasNext: false; nextCursor: undefined };
