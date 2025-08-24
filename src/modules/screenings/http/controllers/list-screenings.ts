173e13f781ad1f3f4c42120d5196038c140dbce4
Raphael Passos <raphael251@hotmail.com>
2025-08-24 20:56:15 -0300

    chore(screenings): add generic error handling in the listing endpoint

--- src/modules/screenings/http/controllers/list-screenings.ts
+++ src/modules/screenings/http/controllers/list-screenings.ts
@@ -7,29 +7,34 @@ export class ListScreeningsController implements IHttpControllerV2<Screening[]> 
   constructor(private screeningRepository: IScreeningRepository) {}
 
   async handle(request: THttpRequest): Promise<THttpResponse<Screening[]>> {
-    const requestQuerySchema = z.object({
-      cursor: z.string().min(1).optional(),
-      limit: z.coerce.number().min(1).optional(),
-    });
+    try {
+      const requestQuerySchema = z.object({
+        cursor: z.string().min(1).optional(),
+        limit: z.coerce.number().min(1).optional(),
+      });
 
-    const { success: isValid, data: pagination } = requestQuerySchema.safeParse(request.query);
+      const { success: isValid, data: pagination } = requestQuerySchema.safeParse(request.query);
 
-    if (!isValid) {
+      if (!isValid) {
+        return {
+          status: 400,
+          errors: ['Invalid query parameters'],
+        };
+      }
+
+      const { data, hasNext, nextCursor } = await this.screeningRepository.findAll(pagination);
+
       return {
-        status: 400,
-        errors: ['Invalid query parameters'],
+        status: 200,
+        data,
+        meta: {
+          hasNext,
+          nextCursor,
+        },
       };
+    } catch (error) {
+      console.error('Error during screenings listing:', error);
+      return { status: 500 };
     }
-
-    const { data, hasNext, nextCursor } = await this.screeningRepository.findAll(pagination);
-
-    return {
-      status: 200,
-      data,
-      meta: {
-        hasNext,
-        nextCursor,
-      },
-    };
   }
 }
