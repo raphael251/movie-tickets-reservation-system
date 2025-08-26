95b6d2a0d57849894f379ca771f07966a7df6dce
Raphael Passos <raphael251@hotmail.com>
2025-08-25 23:58:51 -0300

    test(reservations): create e2e test covering the reservation creation

---
+++ src/__tests__/e2e/http/reservations/create.ts
@@ -0,0 +1,57 @@
+import request from 'supertest';
+import { createApp } from '../../../../app.ts';
+import { Express } from 'express';
+import { appDataSource } from '../../../../modules/shared/data-source/data-source.ts';
+import { theatersSeed } from '../../../../modules/shared/seed/theaters.ts';
+import { getAdminUserToken, getRegularUserToken } from '../utils/login.ts';
+
+describe('POST /reservations - Create reservation', () => {
+  let testingApp: Express;
+  let adminUserToken: string;
+  let regularUserToken: string;
+  let screeningSeatId: string;
+
+  beforeAll(async () => {
+    testingApp = createApp();
+    await appDataSource.initialize();
+
+    adminUserToken = await getAdminUserToken(testingApp);
+
+    regularUserToken = await getRegularUserToken(testingApp);
+
+    const movieResponse = await request(testingApp).post('/movies').set('authorization', `Bearer ${adminUserToken}`).send({
+      title: 'Cars',
+      description: 'The story of Lightning McQueen, a race car who ends up in a small town and learns valuable life lessons.',
+      category: 'animation',
+    });
+
+    const screeningResponse = await request(testingApp).post('/screenings').set('authorization', `Bearer ${adminUserToken}`).send({
+      movieId: movieResponse.body.data.id,
+      theaterId: theatersSeed[0].id,
+      startTime: '2025-08-21T23:41',
+      endTime: '2025-08-22T00:40',
+    });
+
+    const screeningSeats = await request(testingApp)
+      .get(`/screenings/${screeningResponse.body.data.id}/seats`)
+      .set('authorization', `Bearer ${regularUserToken}`)
+      .send();
+
+    screeningSeatId = screeningSeats.body.data[0].id;
+  });
+
+  it('should create a reservation successfully', async () => {
+    const response = await request(testingApp).post('/reservations').set('authorization', `Bearer ${regularUserToken}`).send({
+      screeningSeatId,
+    });
+
+    expect(response.status).toBe(201);
+    expect(response.body).toEqual({
+      data: {
+        id: expect.any(String),
+        seat: 'A1',
+        status: 'CONFIRMED',
+      },
+    });
+  });
+});
