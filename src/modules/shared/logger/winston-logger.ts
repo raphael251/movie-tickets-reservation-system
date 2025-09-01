88d95af0b77373b9dd966d233c0383fc1beebecc
Raphael Passos <raphael251@hotmail.com>
2025-08-31 21:52:23 -0300

    chore(logger): add logger winston implementation

---
+++ src/modules/shared/logger/winston-logger.ts
@@ -0,0 +1,27 @@
+import { ILogger } from './interfaces/logger';
+import winston from 'winston';
+
+export class WinstonLogger implements ILogger {
+  private logger: winston.Logger;
+
+  constructor() {
+    this.logger = winston.createLogger({
+      format: winston.format.json(),
+      transports: [new winston.transports.Console()],
+    });
+  }
+
+  info(message: string, data?: unknown): void {
+    this.logger.info(message, data);
+  }
+  error(message: string, data?: unknown): void {
+    if (data && typeof data === 'object' && 'error' in data && data.error instanceof Error) {
+      data.error = data.error.stack;
+    }
+
+    this.logger.error(message, data);
+  }
+  warn(message: string, data?: unknown): void {
+    this.logger.warn(message, data);
+  }
+}
