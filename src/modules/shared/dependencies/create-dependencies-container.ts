349de9d3df6afa298ef591f1f455436140f152af
Raphael Passos <raphael251@hotmail.com>
2025-09-02 21:12:18 -0300

    refactor(movies): move dependency injection bindings to modules' folder

--- src/modules/shared/dependencies/create-dependencies-container.ts
+++ src/modules/shared/dependencies/create-dependencies-container.ts
@@ -1,14 +1,7 @@
 import { Container } from 'inversify';
-import { CreateMovieController } from '../../movies/http/controllers/create.ts';
-import { DeleteMovieController } from '../../movies/http/controllers/delete.ts';
-import { ListMoviesController } from '../../movies/http/controllers/list.ts';
-import { UpdateMovieController } from '../../movies/http/controllers/update.ts';
-import { MovieRepository } from '../../movies/repositories/movie.repository.ts';
-import { CreateMovieUseCase } from '../../movies/use-cases/create.ts';
-import { DeleteMovieUseCase } from '../../movies/use-cases/delete.ts';
-import { UpdateMovieUseCase } from '../../movies/use-cases/update.ts';
 import { AppConfig } from '../configs/app-config.ts';
 import { WinstonLogger } from '../logger/winston-logger.ts';
+import { createDependenciesContainer as createMoviesDependenciesContainer } from '../../movies/dependencies/create-dependencies-container.ts';
 import { createDependenciesContainer as createReservationsDependenciesContainer } from '../../reservations/dependencies/create-dependencies-container.ts';
 import { createDependenciesContainer as createScreeningsDependenciesContainer } from '../../screenings/dependencies/create-dependencies-container.ts';
 import { createDependenciesContainer as createTheatersDependenciesContainer } from '../../theaters/dependencies/create-dependencies-container.ts';
@@ -18,24 +11,11 @@ import { Hasher } from '../security/hasher.ts';
 export function createDependenciesContainer(appConfig: AppConfig): Container {
   const container = new Container();
 
+  createMoviesDependenciesContainer(container);
   createReservationsDependenciesContainer(container);
   createScreeningsDependenciesContainer(container);
   createTheatersDependenciesContainer(container);
   createUsersDependenciesContainer(container);
-
-  // Controllers - movies
-  container.bind(CreateMovieController).toSelf();
-  container.bind(DeleteMovieController).toSelf();
-  container.bind(ListMoviesController).toSelf();
-  container.bind(UpdateMovieController).toSelf();
-
-  // Use Cases
-  container.bind(CreateMovieUseCase).toSelf();
-  container.bind(UpdateMovieUseCase).toSelf();
-  container.bind(DeleteMovieUseCase).toSelf();
-
-  // Repositories
-  container.bind(MovieRepository).toSelf();
 
   // Logger
   container.bind(WinstonLogger).toSelf();
