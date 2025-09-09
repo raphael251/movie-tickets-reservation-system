988d6264d1997aec6a4b16423b5541a323f93a23
Raphael Passos <raphael251@hotmail.com>
2025-09-08 22:15:58 -0300

    refactor(movies): move express routes to the modules' folder

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,13 @@ import express, { Request, Response } from 'express';
 import { UsersSignUpController } from './modules/users/http/controllers/sign-up.ts';
 import { AppConfigLoader } from './modules/shared/configs/app-config.ts';
 import { UsersLoginController } from './modules/users/http/controllers/login.ts';
-import { expressAuthMiddleware } from './modules/shared/external/express/middlewares/auth-middleware.ts';
-import { JWTTokenValidator } from './modules/shared/security/token-validator.ts';
-import { expressRequiredPermissionsMiddleware } from './modules/shared/external/express/middlewares/required-permissions.ts';
-import { CreateMovieController } from './modules/movies/http/controllers/create.ts';
-import { UpdateMovieController } from './modules/movies/http/controllers/update.ts';
-import { ListMoviesController } from './modules/movies/http/controllers/list.ts';
-import { DeleteMovieController } from './modules/movies/http/controllers/delete.ts';
 import { expressHttpControllerAdapter } from './modules/shared/external/express/adapters/controller-adapter.ts';
 import { Container } from 'inversify';
 import { createDependenciesContainer } from './modules/shared/dependencies/create-dependencies-container.ts';
 import cors from 'cors';
 import { reservationsRouter } from './modules/reservations/external/express/router.ts';
 import { screeningsRouter } from './modules/screenings/external/express/router.ts';
+import { moviesRouter } from './modules/movies/external/express/router.ts';
 
 const appConfig = AppConfigLoader.load();
 
@@ -34,33 +28,7 @@ export function createApp() {
 
   app.post('/users/login', expressHttpControllerAdapter(container.get(UsersLoginController)));
 
-  app.post(
-    '/movies',
-    expressRequiredPermissionsMiddleware(['movies:create']),
-    expressAuthMiddleware(new JWTTokenValidator(appConfig)),
-    expressHttpControllerAdapter(container.get(CreateMovieController)),
-  );
-
-  app.get(
-    '/movies',
-    expressRequiredPermissionsMiddleware(['movies:read']),
-    expressAuthMiddleware(new JWTTokenValidator(appConfig)),
-    expressHttpControllerAdapter(container.get(ListMoviesController)),
-  );
-
-  app.put(
-    '/movies/:movieId',
-    expressRequiredPermissionsMiddleware(['movies:update']),
-    expressAuthMiddleware(new JWTTokenValidator(appConfig)),
-    expressHttpControllerAdapter(container.get(UpdateMovieController)),
-  );
-
-  app.delete(
-    '/movies/:movieId',
-    expressRequiredPermissionsMiddleware(['movies:delete']),
-    expressAuthMiddleware(new JWTTokenValidator(appConfig)),
-    expressHttpControllerAdapter(container.get(DeleteMovieController)),
-  );
+  app.use(moviesRouter(container, appConfig));
 
   app.use(screeningsRouter(container, appConfig));
 
