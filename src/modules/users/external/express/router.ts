05767866789511f6edcdf3f2a32bf085c4f33574
Raphael Passos <raphael251@hotmail.com>
2025-09-12 19:10:18 -0300

    chore(swagger): add users docs

--- src/modules/users/external/express/router.ts
+++ src/modules/users/external/express/router.ts
@@ -7,8 +7,74 @@ import { UsersSignUpController } from '../../http/controllers/sign-up.ts';
 export function usersRouter(container: Container): Router {
   const app = Router();
 
+  /**
+   * @swagger
+   * components:
+   *  schemas:
+   *    UserSignUpInput:
+   *      type: object
+   *      properties:
+   *        email:
+   *          type: string
+   *        password:
+   *          type: string
+   *    UserLoginInput:
+   *      type: object
+   *      properties:
+   *        email:
+   *          type: string
+   *        password:
+   *          type: string
+   *    UserLoginOutput:
+   *      type: object
+   *      properties:
+   *        data:
+   *          type: object
+   *          properties:
+   *            token:
+   *              type: string
+   */
+
+  /**
+   * @swagger
+   * /users:
+   *   post:
+   *     summary: User sign up.
+   *     tags:
+   *      - Users Authentication
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             $ref: "#/components/schemas/UserSignUpInput"
+   *     responses:
+   *       '201':
+   *         description: The user was created successfuly.
+   */
   app.post('/users', expressHttpControllerAdapter(container.get(UsersSignUpController)));
 
+  /**
+   * @swagger
+   * /users/login:
+   *   post:
+   *     summary: User login.
+   *     tags:
+   *      - Users Authentication
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             $ref: "#/components/schemas/UserLoginInput"
+   *     responses:
+   *       '201':
+   *         description: The user was created successfuly.
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: "#/components/schemas/UserLoginOutput"
+   */
   app.post('/users/login', expressHttpControllerAdapter(container.get(UsersLoginController)));
 
   return app;
