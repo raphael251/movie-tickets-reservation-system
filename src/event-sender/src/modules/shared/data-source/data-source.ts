876685b710bf18de36bf5e41b43328260e36b311
Raphael Passos <raphael251@hotmail.com>
2025-09-21 17:06:08 -0300

    feat(event-sender): create event-sender worker initial structure withing the monorepo

---
+++ src/event-sender/src/modules/shared/data-source/data-source.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+import { AppConfigLoader } from '../../shared/configs/app-config.ts';
+import { readFileSync } from 'node:fs';
+import { Outbox } from '../../events/database/outbox.entity.ts';
+
+const appConfig = AppConfigLoader.load();
+
+export const appDataSource = new DataSource({
+  type: 'postgres',
+  host: appConfig.DB_HOST,
+  port: appConfig.DB_PORT,
+  username: appConfig.DB_USERNAME,
+  password: appConfig.DB_PASSWORD,
+  database: appConfig.DB_DATABASE,
+  ssl: appConfig.DB_SSL_CA_PATH
+    ? {
+        rejectUnauthorized: false,
+        ca: readFileSync(appConfig.DB_SSL_CA_PATH, 'utf-8'),
+      }
+    : false,
+  entities: [Outbox],
+  migrations: [appConfig.DB_MIGRATIONS_PATH],
+  logging: appConfig.DB_LOGGING_ENABLED,
+  useUTC: true,
+});
